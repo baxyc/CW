be66672dd7a4a97e066d7e93d8f3729ffd4947c6
Ivan <baxyc.da@gmail.com>
2022-09-06 10:44:11 +1100

    1

--- letsgo/Program.cs
+++ letsgo/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] resultArray(string[] arr)
+{
+    int size = 0;
+    for (int i = 0; i < arr.Length; i++) 
+    {
+        if (arr[i].Length < 4)
+        {
+            size++; 
+        }
+    }
+    string[] newArray = new string[size]; 
+    int index = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length < 4)
+        {
+            newArray[index] = arr[i];
+            index++;
+        }
+    }
+    return newArray;
+}
+
+string[] array = { "123", "ABC", "RUS", "ENG", "GG", "WP" };
+string[] array1 = { "987", "АБВ", "Sister", "Brother", "Mom", "Dad" };
+
+Console.WriteLine(String.Join(", ", resultArray(array)));
+Console.WriteLine(String.Join(", ", resultArray(array1)));
